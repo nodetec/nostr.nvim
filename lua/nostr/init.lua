2dd9c34292c2d9c14e27c458feb5bba7fb769fbd
Christian Chiarulli <chris.machine@pm.me>
2023-07-28 12:05:26 -0400

    feat: basic reading form relay

--- lua/nostr/init.lua
+++ lua/nostr/init.lua
@@ -1,17 +1,83 @@
 local M = {}
 
+M.opts = {}
+
 function M.setup(opts)
-  local plugin_opts = {
-    option1 = opts.option1 or "default_value1",
-    option2 = opts.option2 or "default_value2",
-  }
-  return plugin_opts
+	M.opts = {
+		relay = opts.relay or "default_value1",
+		option2 = opts.option2 or "default_value2",
+	}
 end
 
-function M.my_command()
-  print "MyPlugin command executed!"
+function M.read_from_relay()
+	-- print("MyPlugin command executed!")
+	-- TODO: pass tags
+	local result = vim.fn["ReadFromRelay"]("TESTING")
+	print(result)
 end
 
-vim.api.nvim_set_keymap("n", "<c-a>", '<cmd>lua require("nostr").my_command()<cr>', { noremap = true })
+function M.show_notes(eventsStr)
+	local Popup = require("nui.popup")
+	local event = require("nui.utils.autocmd").event
+
+	local popup = Popup({
+		enter = true,
+		focusable = true,
+		border = {
+			style = "rounded",
+			text = {
+				top = " Notes ",
+				top_align = "center",
+			},
+		},
+		position = "50%",
+		size = {
+			width = "60%",
+			height = "60%",
+		},
+		buf_options = {
+			modifiable = true,
+			readonly = false,
+			filetype = "nostr_notes",
+		},
+		win_options = {
+			-- winblend = 10,
+			winhighlight = "Normal:Normal,FloatBorder:Constant",
+			wrap = true,
+		},
+	})
+
+	popup:mount()
+
+	popup:on(event.BufLeave, function()
+		popup:unmount()
+	end)
+
+	popup:map("n", "q", function()
+		-- vim.cmd("bd!")
+		popup:hide()
+	end, { noremap = true })
+
+	popup:map("n", "<s-l>", function()
+		popup.border:set_text("bottom", " 2/3 ", "right")
+	end, { noremap = true })
+
+	local events = vim.fn.json_decode(eventsStr)
+	local lines = {}
+
+	if events == nil then
+		print("No events found")
+		return
+	end
+
+	for _, e in ipairs(events) do
+		local result = e.content:gsub("\n", "")
+		table.insert(lines, result)
+		table.insert(lines, "")
+		print(e.content)
+	end
+
+	vim.api.nvim_buf_set_lines(popup.bufnr, 0, -1, false, lines)
+end
 
 return M
--- rplugin/node/nostr/index.js
+++ rplugin/node/nostr/index.js
@@ -1,131 +1,76 @@
 const nostr = require("nostr-tools");
 require("websocket-polyfill");
-
-const generatePrivateKey = nostr.generatePrivateKey;
-const getPublicKey = nostr.getPublicKey;
-const initializeRelay = nostr.relayInit;
-const getEventHash = nostr.getEventHash;
-// NOTE: this function will soon be deprecated
-const signEvent = nostr.signEvent;
-// const getSignature = nostr.getSignature;
-
 const fs = require("fs");
 
-const HOME = process.env.HOME;
-const NOSTR_DEV_MODE = process.env.NOSTR_DEV_MODE;
 
-let dev = false;
-if (NOSTR_DEV_MODE) {
-  dev = true;
-}
-
-
-// HACK: this is a hack to get logging working
 function logme(msg) {
-  fs.appendFile(HOME + "/.local/state/nostr.log", msg + "\n", (err) => {
+  fs.appendFile("/Users/chris" + "/.local/state/nostr.log", msg + "\n", (err) => {
     if (err) throw err;
   });
 }
 
-const readConfig = () => {
-  fs.readFile(HOME + "/.config/nostr-nvim/config.json", (err, data) => {
-    if (err) throw err;
-    let student = JSON.parse(data);
-    logme(student);
-  });
 
-  return student;
+module.exports = plugin => {
+  plugin.setOptions({ dev: true });
+
+  plugin.registerFunction('ReadFromRelay', async (test) => {
+    try {
+      logme("====================================");
+      const relay = nostr.relayInit("wss://relay.damus.io");
+
+      relay.on("connect", () => {
+        logme(`connected to ${relay.url}`);
+      });
+
+      relay.on("error", () => {
+        logme(`failed to connect to ${relay.url}`);
+      });
+
+      await relay.connect();
+
+      let events = []
+
+      let sub = relay.sub([
+        {
+          "kinds": [1],
+          limit: 5,
+        },
+      ]);
+
+      sub.on("event", (event) => {
+        events.push(event)
+      });
+
+      sub.on("eose", () => {
+        try {
+          plugin.nvim.setVar('events', JSON.stringify(events));
+        } catch (err) {
+          logme("error: " + err);
+        }
+
+        plugin.nvim.command(`lua require("nostr").show_notes(vim.g.events)`);
+        sub.unsub();
+      });
+
+
+    } catch (err) {
+      console.error(err);
+    }
+  }, { sync: false });
+
+  plugin.registerFunction('PublishToRelay', () => {
+
+    let sk = nostr.generatePrivateKey();
+    let pk = nostr.getPublicKey(sk);
+    logme(test)
+
+    logme("private key " + sk);
+    logme("public key " + pk);
+
+    logme("PublishToRelay");
+    return 'May I offer you an egg in these troubling times'
+    // .then(() => logme('Line should be set'))
+  }, { sync: true })
+
 };
 
-module.exports = (plugin) => {
-  plugin.setOptions({ dev: dev });
-
-  plugin.registerCommand(
-    "ReadFromRelay",
-    async () => {
-      try {
-        logme("before");
-        const relay = initializeRelay("wss://nostr-pub.wellorder.net");
-        logme("relay initialized");
-        relay.on("connect", () => {
-          logme(`connected to ${relay.url}`);
-        });
-        logme("after connect");
-        relay.on("error", () => {
-          logme(`failed to connect to ${relay.url}`);
-        });
-
-        await relay.connect();
-
-        // let's query for an event that exists
-        let sub = relay.sub([
-          {
-            ids: [
-              "d7dd5eb3ab747e16f8d0212d53032ea2a7cadef53837e5a6c66d42849fcb9027",
-            ],
-          },
-        ]);
-        sub.on("event", (event) => {
-          logme("we got the event we wanted: " + JSON.stringify(event));
-        });
-        sub.on("eose", () => {
-          sub.unsub();
-        });
-      } catch (err) {
-        console.error(err);
-      }
-    },
-    { sync: false }
-  );
-
-  plugin.registerFunction(
-    "PublishToRelay",
-    async () => {
-      try {
-        // logme("test message is: " + msg);
-        const relay = initializeRelay("wss://nostr-pub.wellorder.net");
-        relay.on("connect", () => {
-          logme(`connected to ${relay.url}`);
-        });
-        relay.on("error", () => {
-          logme(`failed to connect to ${relay.url}`);
-        });
-
-        logme("before connect");
-        await relay.connect();
-        logme("after connect");
-
-        // let's publish a new event while simultaneously monitoring the relay for it
-        let sk = generatePrivateKey();
-        let pk = getPublicKey(sk);
-        logme("pk is: " + pk);
-        logme("sk is: " + sk);
-
-        let event = {
-          kind: 1,
-          pubkey: pk,
-          created_at: Math.floor(Date.now() / 1000),
-          tags: [],
-          content: "hello from neovim town",
-        };
-        event.id = getEventHash(event);
-        event.sig = signEvent(event, sk);
-
-        logme("event is: " + JSON.stringify(event));
-
-        let pub = relay.publish(event);
-        pub.on("ok", () => {
-          logme(`${relay.url} has accepted our event`);
-        });
-        pub.on("failed", (reason) => {
-          logme(`failed to publish to ${relay.url}: ${reason}`);
-        });
-
-        relay.close();
-      } catch (err) {
-        logme("failed to send message message: " + msg);
-      }
-    },
-    { sync: false }
-  );
-};
