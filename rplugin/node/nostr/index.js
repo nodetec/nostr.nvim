be1bfe24e88081a6c7605fdd3818d3c693dff8de
Christian Chiarulli <chris.machine@pm.me>
2023-10-04 14:19:32 -0400

    update formatting

--- rplugin/node/nostr/index.js
+++ rplugin/node/nostr/index.js
@@ -1,27 +1,46 @@
-const keys = require('./lib/keys');
-const relayManager = require('./lib/relayManager');
+const keys = require("./lib/keys");
+const relayManager = require("./lib/relayManager");
 
-module.exports = plugin => {
+module.exports = (plugin) => {
   plugin.setOptions({ dev: true });
 
-  plugin.registerFunction('NostrGenerateKeys', async () => {
-    keys.getKeyPair();
-  }, { sync: false });
+  plugin.registerFunction(
+    "NostrGenerateKeys",
+    async () => {
+      keys.getKeyPair();
+    },
+    { sync: false },
+  );
 
-  plugin.registerFunction('NostrAddRelay', async (url) => {
-    relayManager.addRelay(url, plugin);
-  }, { sync: false });
+  plugin.registerFunction(
+    "NostrAddRelay",
+    async (url) => {
+      relayManager.addRelay(url, plugin);
+    },
+    { sync: false },
+  );
 
-  plugin.registerFunction('NostrRemoveRelay', async (url) => {
-    relayManager.removeRelay(url);
-  }, { sync: false });
+  plugin.registerFunction(
+    "NostrRemoveRelay",
+    async (url) => {
+      relayManager.removeRelay(url);
+    },
+    { sync: false },
+  );
 
-  plugin.registerFunction('NostrListRelays', async () => {
-    return relayManager.listRelays(plugin);
-  }, { sync: false });
+  plugin.registerFunction(
+    "NostrListRelays",
+    async () => {
+      return relayManager.listRelays(plugin);
+    },
+    { sync: false },
+  );
 
-  plugin.registerFunction('NostrSetActiveRelay', async (url) => {
-    await relayManager.setActiveRelay(url, plugin);
-  }, { sync: false });
+  plugin.registerFunction(
+    "NostrSetActiveRelay",
+    async (url) => {
+      await relayManager.setActiveRelay(url, plugin);
+    },
+    { sync: false },
+  );
 };
-
